8d8fe837a0da9c7aa42ecee46d03b6b55b84f72f
Zachary Lang <langzg@dukes.jmu.edu>
2020-10-20 21:58:38 -0400

    C# Onclick Event for adding a Volunteer - Should work properly based on the new DB files on google drive

--- CyberCity/VolunteerAccount.aspx.cs
+++ CyberCity/VolunteerAccount.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
 
 namespace CyberCity
 {
@@ -16,6 +19,46 @@ namespace CyberCity
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
+            string connectionString;
+            SqlConnection cnn;
+            SqlCommand sqlCommand;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            connectionString = WebConfigurationManager.ConnectionStrings["CYBERCITY"].ToString();
+
+            cnn = new SqlConnection(connectionString);
+
+            String sql = "Insert into [Volunteer] (FName, LName, TShirtSize, Organization, DOB, Email, Phone, LunchTicket, Gender) " +
+                "Values (@FirstName, @LastName, @ShirtSize, @OrgName, @DOB, @Email, @Phone, @LunchTicket, @Gender)";
+
+            sqlCommand = new SqlCommand(sql, cnn);
+
+            cnn.Open();
+
+
+            //Inserts the data from the new student page into the database
+
+            sqlCommand.Parameters.AddWithValue("@FirstName", HttpUtility.HtmlEncode(txtVolunteerFN.Text));
+            sqlCommand.Parameters.AddWithValue("@LastName", HttpUtility.HtmlEncode(txtVolunteerLN.Text));
+            sqlCommand.Parameters.AddWithValue("@DOB", HttpUtility.HtmlEncode(txtVolunteerDOB.Text));
+
+            //Converts the lunchTicket checkbox to an integer in order to be stored in the database
+            int lunchTicket = 0;
+            if (chkLunch.Checked == true)
+            {
+                lunchTicket = 1;
+            }
+            sqlCommand.Parameters.AddWithValue("@LunchTicket", lunchTicket);
+            sqlCommand.Parameters.AddWithValue("@Phone", HttpUtility.HtmlEncode(txtVolunteerPhone.Text));
+            sqlCommand.Parameters.AddWithValue("@Gender", HttpUtility.HtmlEncode(ddlGender.SelectedValue));
+            sqlCommand.Parameters.AddWithValue("@OrgName", HttpUtility.HtmlEncode(txtOrgName.Text));
+            sqlCommand.Parameters.AddWithValue("@ShirtSize", HttpUtility.HtmlEncode(ddlShirtSize.SelectedValue));
+            sqlCommand.Parameters.AddWithValue("@Email", HttpUtility.HtmlEncode(txtVolunteerEmail.Text));
+
+            sqlCommand.ExecuteNonQuery();
+
+            sqlCommand.Dispose();
+            cnn.Close();
 
         }
     }
