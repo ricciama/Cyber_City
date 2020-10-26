78c7a06f6bfba4f2f3460782e293808439911aa9
ricciama <ricciama@dukes.jmu.edu>
2020-10-26 14:43:19 -0400

    random chiz

--- CyberCity/Site.Master.cs
+++ CyberCity/Site.Master.cs
@@ -13,51 +13,64 @@ namespace CyberCity
         {
 
 
+            //if (Session["UserType"] == null)
+            //{
+            //    SPMenu.Visible = false;
+            //    volunteerMenu.Visible = false;
+            //    orgRepMenu.Visible = false;
+            //    MainMenu.Visible = true;
 
-            if (Session["UserType"] == null)
-            {
-                SPMenu.Visible = false;
-                volunteerMenu.Visible = false;
-                orgRepMenu.Visible = false;
-                MainMenu.Visible = true;
+            //}
+            //else if (Session["UserType"].ToString() == "S")
+            //{
+            //    MainMenu.Visible = false;
+            //    volunteerMenu.Visible = false;
+            //    orgRepMenu.Visible = false;
+            //    SPMenu.Visible = true;
 
-            }
-            else if (Session["UserType"].ToString() == "S")
-            {
-                MainMenu.Visible = false;
-                volunteerMenu.Visible = false;
-                orgRepMenu.Visible = false;
-                SPMenu.Visible = true;
-
-
-            }
-            else if (Session["UserType"].ToString() == "V")
-            {
-                MainMenu.Visible = false;
-                orgRepMenu.Visible = false;
-                SPMenu.Visible = false;
-                volunteerMenu.Visible = true;
-
-            }
-            else if (Session["UserType"].ToString() == "OR")
-            {
-                MainMenu.Visible = false;
-                SPMenu.Visible = true;
-                volunteerMenu.Visible = false;
-                orgRepMenu.Visible = true;
-            }
-            else if (Session["UserType"].ToString() == "C")
-            {
-                MainMenu.Visible = false;
-                SPMenu.Visible = true;
-                volunteerMenu.Visible = false;
-                orgRepMenu.Visible = false;
-                coordinatorMenu.Visible = true;
-            }
+            //    lblSPUser.Text = "Hello, " + Session["Username"].ToString();
 
 
+            //}
+            //else if (Session["UserType"].ToString() == "V")
+            //{
+            //    MainMenu.Visible = false;
+            //    orgRepMenu.Visible = false;
+            //    SPMenu.Visible = false;
+            //    volunteerMenu.Visible = true;
+
+            //    lblVUser.Text = "Hello, " + Session["Username"].ToString();
+
+            //}
+            //else if (Session["UserType"].ToString() == "OR")
+            //{
+            //    MainMenu.Visible = false;
+            //    SPMenu.Visible = true;
+            //    volunteerMenu.Visible = false;
+            //    orgRepMenu.Visible = true;
+
+            //    lblOrgUser.Text = "Hello, " + Session["Username"].ToString();
+            //}
+            //else if (Session["UserType"].ToString() == "C")
+            //{
+            //    MainMenu.Visible = false;
+            //    SPMenu.Visible = true;
+            //    volunteerMenu.Visible = false;
+            //    orgRepMenu.Visible = false;
+            //    coordinatorMenu.Visible = true;
+
+            //    lblCordUser.Text = "Hello, " + Session["Username"].ToString();
+            //}
 
 
+
+
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Response.Redirect("HomePage.aspx");
         }
     }
 }
