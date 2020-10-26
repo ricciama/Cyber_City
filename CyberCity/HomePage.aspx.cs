dee5f309f15b49e11845a5d7d740ac0c99a32292
Katelyn Hinz <hinzkx@dukes.jmu.edu>
2020-10-26 13:10:54 -0400

    home page stufff - not fully finished

--- CyberCity/HomePage.aspx.cs
+++ CyberCity/HomePage.aspx.cs
@@ -16,7 +16,7 @@ namespace CyberCity
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-            Session["UserType"] = txtInput.Text;
         }
+            
     }
 }
--- CyberCity/HomePage.aspx.designer.cs
+++ CyberCity/HomePage.aspx.designer.cs
@@ -7,36 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CyberCity {
-    
-    
-    public partial class _Default {
-        
-        /// <summary>
-        /// Label1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
-        
-        /// <summary>
-        /// txtInput control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtInput;
-        
-        /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+namespace CyberCity
+{
+
+
+    public partial class _Default
+    {
     }
 }
