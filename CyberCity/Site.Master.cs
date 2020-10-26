57587e36cf1bd6ac636253fe63ee5202b136b764
ricciama <ricciama@dukes.jmu.edu>
2020-10-26 14:53:09 -0400

    poop

--- CyberCity/Site.Master.cs
+++ CyberCity/Site.Master.cs
@@ -13,54 +13,54 @@ namespace CyberCity
         {
 
 
-            //if (Session["UserType"] == null)
-            //{
-            //    SPMenu.Visible = false;
-            //    volunteerMenu.Visible = false;
-            //    orgRepMenu.Visible = false;
-            //    MainMenu.Visible = true;
+            if (Session["UserType"] == null)
+            {
+                SPMenu.Visible = false;
+                volunteerMenu.Visible = false;
+                orgRepMenu.Visible = false;
+                MainMenu.Visible = true;
 
-            //}
-            //else if (Session["UserType"].ToString() == "S")
-            //{
-            //    MainMenu.Visible = false;
-            //    volunteerMenu.Visible = false;
-            //    orgRepMenu.Visible = false;
-            //    SPMenu.Visible = true;
+            }
+            else if (Session["UserType"].ToString() == "S")
+            {
+                MainMenu.Visible = false;
+                volunteerMenu.Visible = false;
+                orgRepMenu.Visible = false;
+                SPMenu.Visible = true;
 
-            //    lblSPUser.Text = "Hello, " + Session["Username"].ToString();
+                lblSPUser.Text = "Hello, " + Session["Username"].ToString();
 
 
-            //}
-            //else if (Session["UserType"].ToString() == "V")
-            //{
-            //    MainMenu.Visible = false;
-            //    orgRepMenu.Visible = false;
-            //    SPMenu.Visible = false;
-            //    volunteerMenu.Visible = true;
+            }
+            else if (Session["UserType"].ToString() == "V")
+            {
+                MainMenu.Visible = false;
+                orgRepMenu.Visible = false;
+                SPMenu.Visible = false;
+                volunteerMenu.Visible = true;
 
-            //    lblVUser.Text = "Hello, " + Session["Username"].ToString();
+                lblVUser.Text = "Hello, " + Session["Username"].ToString();
 
-            //}
-            //else if (Session["UserType"].ToString() == "OR")
-            //{
-            //    MainMenu.Visible = false;
-            //    SPMenu.Visible = true;
-            //    volunteerMenu.Visible = false;
-            //    orgRepMenu.Visible = true;
+            }
+            else if (Session["UserType"].ToString() == "OR")
+            {
+                MainMenu.Visible = false;
+                SPMenu.Visible = true;
+                volunteerMenu.Visible = false;
+                orgRepMenu.Visible = true;
 
-            //    lblOrgUser.Text = "Hello, " + Session["Username"].ToString();
-            //}
-            //else if (Session["UserType"].ToString() == "C")
-            //{
-            //    MainMenu.Visible = false;
-            //    SPMenu.Visible = true;
-            //    volunteerMenu.Visible = false;
-            //    orgRepMenu.Visible = false;
-            //    coordinatorMenu.Visible = true;
+                lblOrgUser.Text = "Hello, " + Session["Username"].ToString();
+            }
+            else if (Session["UserType"].ToString() == "C")
+            {
+                MainMenu.Visible = false;
+                SPMenu.Visible = true;
+                volunteerMenu.Visible = false;
+                orgRepMenu.Visible = false;
+                coordinatorMenu.Visible = true;
 
-            //    lblCordUser.Text = "Hello, " + Session["Username"].ToString();
-            //}
+                lblCordUser.Text = "Hello, " + Session["Username"].ToString();
+            }
 
 
 
