3ef142d65979251f1f70d14ca9158fb9b296be0b
Zachary Lang <langzg@dukes.jmu.edu>
2020-10-28 12:41:36 -0400

    Adds the FirstTimeLogin Page for a volunteer and checks to see if it is a volunteers first time logging in when loading the homepage

---
+++ CyberCity/FirstTimeLoginVolunteer.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace CyberCity
+{
+    public partial class FirstTimeLoginVolunteer : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            String sqlUpdate = "UPDATE Volunteer SET [LunchTicket] = @LunchTicket, [Phone] = @Phone, [Gender] = @Gender, [TShirtSize] = @TshirtSize " +
+                "WHERE Username = ('" + Session["Username"].ToString() + "')";
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            SqlConnection sqlConnection = new SqlConnection(WebConfigurationManager.ConnectionStrings["CyberCity"].ConnectionString.ToString());
+            SqlCommand sqlCommand = new SqlCommand(sqlUpdate, sqlConnection);
+         
+            sqlCommand.Parameters.AddWithValue("@Phone", HttpUtility.HtmlEncode(txtVolunteerPhone.Text));
+            sqlCommand.Parameters.AddWithValue("@Gender", ddlGender.SelectedValue);
+
+            int lunchTicket = 0;
+            if (chkLunch.Checked == true)
+            {
+                lunchTicket = 1;
+            }
+            sqlCommand.Parameters.AddWithValue("@LunchTicket", lunchTicket);
+            sqlCommand.Parameters.AddWithValue("@TshirtSize", ddlShirtSize.SelectedValue);
+
+            sqlConnection.Open();
+
+            adapter.UpdateCommand = sqlCommand;
+            adapter.UpdateCommand.ExecuteNonQuery();
+
+            sqlCommand.Dispose();
+
+            sqlConnection.Close();
+
+            Response.Redirect("HomePage.aspx");
+        }
+    }
+}
--- CyberCity/HomePage.aspx.cs
+++ CyberCity/HomePage.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using Microsoft.Ajax.Utilities;
 
 namespace CyberCity
 {
@@ -11,7 +15,25 @@ namespace CyberCity
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(Session["UserType"].ToString().Equals("V"))
+            {
+                SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["CYBERCITYDB"].ConnectionString.ToString());
 
+                sc.Open();
+
+                String sqlQuery = "Select TShirtSize from [Volunteer] where [Username] = @Username";
+
+                SqlCommand sqlCommand1 = new SqlCommand(sqlQuery, sc);
+
+                sqlCommand1.Parameters.AddWithValue("@Username", Session["Username"].ToString());
+
+                string TShirtSize = sqlCommand1.ExecuteScalar().ToString();
+
+                if(!TShirtSize.IsNullOrWhiteSpace())
+                {
+                    Response.Redirect("FirstTimeLoginVolunteer.aspx");
+                }
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
