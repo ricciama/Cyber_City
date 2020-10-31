97cd619270a6657ef27946eb3462e8a887ad8400
ricciama <ricciama@dukes.jmu.edu>
2020-10-31 17:47:11 -0400

    register org rep page

---
+++ CyberCity/RegisterOrgRep.aspx
@@ -0,0 +1,123 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="RegisterOrgRep.aspx.cs" Inherits="CyberCity.RegisterOrgRep" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+    <link href="css/CreatingEntity.css" rel="stylesheet" />
+    <link href="Content/bootstrap.css" rel="stylesheet" />
+   
+        <br />
+    <br />
+    <asp:Panel ID="pnlRegisterOrg" runat="server"  BorderColor="#cccccc" BorderStyle="Solid">
+        <br />
+        <br />
+        <asp:Table ID="tblRegisterOrg" runat="server" HorizontalAlign="Center" CellPadding="50" CellSpacing="50">
+
+            <asp:TableRow HorizontalAlign="Center" >
+                <asp:TableCell ColumnSpan="2">
+                    <img width="100" src="Images/loginguy.png"/>
+                </asp:TableCell>            
+            </asp:TableRow>
+            <asp:TableRow HorizontalAlign="Center">
+                <asp:TableCell ColumnSpan="2">                    
+                    <h4>Register an Orgsnizational Representative for an Event</h4>              
+                </asp:TableCell>
+            </asp:TableRow>
+            <%-- Select Program --%>
+            <asp:TableRow HorizontalAlign="Center" CssClass="cellPadding">
+                <asp:TableCell  HorizontalAlign="Right" CssClass="cellPadding">                    
+                    <div class="form-group">
+                        <asp:Label ID="lblProgram" runat="server" Text="Select Program"></asp:Label>
+                    </div>
+                </asp:TableCell>
+                <asp:TableCell HorizontalAlign="Center" CssClass="cellPadding">             
+                    <div class="form-group">
+                        
+                        <asp:UpdatePanel runat="server" ID="beans">
+                            <ContentTemplate>
+                                    <asp:DropDownList ID="ddlSelectProgram" runat="server" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged = "ddl_Event_SelectedIndexChanged"></asp:DropDownList>
+                            </ContentTemplate>
+                        </asp:UpdatePanel>
+                    </div>
+                </asp:TableCell>
+            </asp:TableRow>
+            <%-- Select Organization --%>
+            <asp:TableRow HorizontalAlign="Center" CssClass="cellPadding">
+                <asp:TableCell HorizontalAlign="Right" CssClass="cellPadding">
+                    <div class="form-group">
+                        <asp:Label ID="lblSelectOrg" runat="server" Text="Select Organization"></asp:Label>
+                    </div>
+                </asp:TableCell>
+                <asp:TableCell HorizontalAlign="Center" CssClass="cellPadding">
+                    <div class="form-group">
+                        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+                            <ContentTemplate>
+                                <asp:DropDownList ID="ddlSelectOrg" runat="server" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="ddl_OrgRep_SelectedIndexChanged"></asp:DropDownList>
+                            </ContentTemplate>
+                        </asp:UpdatePanel>
+                    </div>
+                </asp:TableCell>
+            </asp:TableRow>
+            <%-- Select Org Rep --%>
+            <asp:TableRow HorizontalAlign="Center" CssClass="cellPadding">
+                <asp:TableCell HorizontalAlign="Right" CssClass="cellPadding">
+                    <div class="form-group">
+                        <asp:Label ID="lblOrgRep" runat="server" Text="Select Organizational Representative"></asp:Label>
+                    </div>                   
+                </asp:TableCell>
+                <asp:TableCell HorizontalAlign="Center" CssClass="cellPadding">
+                    <div class="form-group">
+                        <asp:UpdatePanel ID="UpdatePanel2" runat="server">
+                            <ContentTemplate>
+                                <asp:DropDownList ID="ddlOrgRep" runat="server" CssClass="form-control"></asp:DropDownList>
+                            </ContentTemplate>
+                        </asp:UpdatePanel>
+                    </div>                    
+                </asp:TableCell>
+            </asp:TableRow>
+            <%-- Select Event --%>
+            <asp:TableRow HorizontalAlign="Center" CssClass="cellPadding">
+                <asp:TableCell HorizontalAlign="Right" CssClass="cellPadding">
+                    <div class="form-group">
+                        <asp:Label ID="lblEvent" runat="server" Text="Select Event"></asp:Label>
+                    </div>
+                </asp:TableCell>
+                <asp:TableCell HorizontalAlign="Center" CssClass="cellPadding">
+                    <div class="form-group">
+                        <asp:UpdatePanel ID="UpdatePanel3" runat="server">
+                            <ContentTemplate>
+                                <asp:DropDownList ID="ddlEvent" runat="server" CssClass="form-control"></asp:DropDownList>
+                            </ContentTemplate>
+                        </asp:UpdatePanel>
+                    </div>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow HorizontalAlign="Center" CssClass="cellPadding">
+                <asp:TableCell HorizontalAlign="Center" ColumnSpan="2">
+                    <div class="form-group">
+                         <asp:Button class="btn btn-primary btn-block btn-lg" ID="btnRegister" runat="server" Text="Register" OnClick="btnRegister_Click" />
+                    </div>
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+
+            <br />
+            <hr />
+            <br />
+
+        <%-- Table to Display Program Schedule --%>
+        <div class="container">
+            <div class="floatLeft">
+                <asp:GridView 
+                    runat="server" 
+                    ID="programSchedule" 
+                    AlternatingRowStyle-BackColor="MediumPurple" 
+                    BorderColor="Black">         
+                </asp:GridView>
+            </div>
+        </div>      
+
+    </asp:Panel>
+
+
+
+
+</asp:Content>
---
+++ CyberCity/RegisterOrgRep.aspx.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.WebSockets;
+
+namespace CyberCity
+{
+    public partial class RegisterOrgRep : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(WebConfigurationManager.ConnectionStrings["CyberCity"].ConnectionString.ToString());
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        SqlDataAdapter da2;
+        DataSet ds = new DataSet();
+        DataSet ds2 = new DataSet();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //populate pogram drop down
+            if (!Page.IsPostBack)
+            {
+                //String sqlQuery2 = "Select ProgramID, Name from Program";
+                //String sqlConnection2 = WebConfigurationManager.ConnectionStrings["CYBERCITY"].ConnectionString;
+
+                //using (SqlConnection con = new SqlConnection(sqlConnection2))
+                //{
+                //    using (SqlCommand cmd = new SqlCommand(sqlQuery2))
+                //    {
+                //        cmd.CommandType = CommandType.Text;
+                //        cmd.Connection = con;
+                //        con.Open();
+                //        using (SqlDataReader sdr = cmd.ExecuteReader())
+                //        {
+                //            while (sdr.Read())
+                //            {
+                //                ListItem item = new ListItem();
+                //                item.Text = sdr["Name"].ToString();
+                //                item.Value = sdr["ProgramID"].ToString();
+                //                ddlSelectProgram.Items.Add(item);
+                //            }
+                //        }
+                //        con.Close();
+                //    }
+                //}
+                //ddlSelectProgram.Items.Insert(0, new ListItem("Select Program", "0"));
+
+                GetProgram();
+                GetOrganization();
+                
+                
+            
+            }
+
+            //// populate organization drop down
+            //if (!Page.IsPostBack)
+            //{
+            //    String orgQuery = "Select OrganizationID, Name from Organization";
+            //    String orgSql = WebConfigurationManager.ConnectionStrings["CYBERCITY"].ConnectionString;
+
+            //    using (SqlConnection con = new SqlConnection(orgSql))
+            //    {
+            //        using (SqlCommand cmd = new SqlCommand(orgQuery))
+            //        {
+            //            cmd.CommandType = CommandType.Text;
+            //            cmd.Connection = con;
+            //            con.Open();
+            //            using (SqlDataReader sdr = cmd.ExecuteReader())
+            //            {
+            //                while (sdr.Read())
+            //                {
+            //                    ListItem item = new ListItem();
+            //                    item.Text = sdr["Name"].ToString();
+            //                    item.Value = sdr["OrganizationID"].ToString();
+            //                    ddlSelectOrg.Items.Add(item);
+            //                }
+            //            }
+            //            con.Close();
+            //        }
+            //    }
+            //    ddlSelectOrg.Items.Insert(0, new ListItem("Select Organization", "0"));
+            //}
+
+            //// populate org rep drop down
+            //int organization = Int32.Parse(ddlSelectOrg.SelectedValue);
+
+            //if (!Page.IsPostBack)
+            //{
+            //    String orgQuery = "SELECT OrgRep.OrgRepID, OrgRep.FName + ' ' + OrgRep.LName as OrganizationlRep FROM Organization INNER JOIN OrgRep ON Organization.OrganizationID = OrgRep.OrganizationID WHERE OrgRep.OrganizationID = " + organization;
+            //    String orgSql = WebConfigurationManager.ConnectionStrings["CYBERCITY"].ConnectionString;
+
+            //    using (SqlConnection con = new SqlConnection(orgSql))
+            //    {
+            //        using (SqlCommand cmd = new SqlCommand(orgQuery))
+            //        {
+            //            cmd.CommandType = CommandType.Text;
+            //            cmd.Connection = con;
+            //            con.Open();
+            //            using (SqlDataReader sdr = cmd.ExecuteReader())
+            //            {
+            //                while (sdr.Read())
+            //                {
+            //                    ListItem item = new ListItem();
+            //                    item.Text = sdr["Name"].ToString();
+            //                    item.Value = sdr["OrgRepID"].ToString();
+            //                    ddlSelectOrg.Items.Add(item);
+            //                }
+            //            }
+            //            con.Close();
+            //        }
+            //    }
+            //    ddlSelectOrg.Items.Insert(0, new ListItem("Select Organization", "0"));
+            //}
+
+
+
+        }
+        private void GetProgram()
+        {
+            String programQuery = "Select ProgramID, Name from Program";
+            da2 = new SqlDataAdapter(programQuery, con);
+            da2.Fill(ds2);
+
+            if (ds2.Tables[0].Rows.Count > 0)
+            {
+                ddlSelectProgram.DataSource = ds2;
+                ddlSelectProgram.DataTextField = "Name";
+                ddlSelectProgram.DataValueField = "ProgramID";
+                ddlSelectProgram.DataBind();
+                ddlSelectProgram.Items.Insert(0, new ListItem("Select Program", "-1"));
+                ddlSelectProgram.SelectedIndex = 0;
+            }
+
+
+
+
+        }
+
+        protected void ddl_Event_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ds2.Clear();
+            string ProgramID;
+            string ProgramName;
+            ProgramName = ddlSelectProgram.SelectedItem.Text;
+            ProgramID = ddlSelectProgram.SelectedValue.ToString();
+
+            if (ProgramID != "-1")
+            {
+                string eventQuery = "SELECT EventID, Name, ProgramID FROM Event WHERE ProgramID = '" + ProgramID.ToString() + "'";
+                da2 = new SqlDataAdapter(eventQuery, con);
+                da2.Fill(ds2);
+                if (ds2.Tables[0].Rows.Count > 0)
+                {
+                    ddlEvent.DataSource = ds2;
+                    ddlEvent.DataTextField = "Name";
+                    ddlEvent.DataValueField = "EventID";
+                    ddlEvent.DataBind();
+                    ddlEvent.Items.Insert(0, new ListItem("Select Event", "-1"));
+                    ddlEvent.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                ddlEvent.Items.Insert(0, "No Events Available");
+                ddlEvent.DataBind();
+            }
+        }
+
+        private void GetOrganization()
+        {
+            String orgQuery = "Select OrganizationID, Name from Organization";
+
+            da = new SqlDataAdapter(orgQuery, con);
+            da.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                ddlSelectOrg.DataSource = ds;
+                ddlSelectOrg.DataTextField = "Name";
+                ddlSelectOrg.DataValueField = "OrganizationID";
+                ddlSelectOrg.DataBind();
+                ddlSelectOrg.Items.Insert(0, new ListItem("Select Organization", "-1"));
+                ddlSelectOrg.SelectedIndex = 0;
+            }
+
+
+        }
+
+        protected void ddl_OrgRep_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ds.Clear();
+            string OrgID;
+            string OrgName;
+            OrgName = ddlSelectOrg.SelectedItem.Text;
+            OrgID = ddlSelectOrg.SelectedValue.ToString();
+
+
+
+            if (OrgID != "-1")
+            {
+                string orgRepQuery = "SELECT OrgRepID, FName + ' ' + LName AS OrganizationalRep, OrganizationID FROM OrgRep WHERE OrganizationID = '" + OrgID.ToString() + "'";
+                da = new SqlDataAdapter(orgRepQuery, con);
+                da.Fill(ds);
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    ddlOrgRep.DataSource = ds;
+                    ddlOrgRep.DataTextField = "OrganizationalRep";
+                    ddlOrgRep.DataValueField = "OrgRepID";
+                    ddlOrgRep.DataBind();
+                    ddlOrgRep.Items.Insert(0, new ListItem("Select Organizational Rep", "-1"));
+                    ddlOrgRep.SelectedIndex = 0;
+                }
+            }
+            else
+            {
+                ddlOrgRep.Items.Insert(0, "No Organizational Reps Available");
+                ddlOrgRep.DataBind();
+            }
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
